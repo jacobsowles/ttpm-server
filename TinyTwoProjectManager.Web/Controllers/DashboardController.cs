cb08072d238f9ceaf51c6d11621b8554036cc0eb
jacob.sowles <jacob.sowles@speakcreative.com>
2016-08-04 16:22:48 -0700

    refactor: remove iteration to build DashboardViewModel (AutoMapper does this automatically)

--- TinyTwoProjectManager.Web/Controllers/DashboardController.cs
+++ TinyTwoProjectManager.Web/Controllers/DashboardController.cs
@@ -10,34 +10,16 @@ namespace TinyTwoProjectManager.Web.Controllers
     public class DashboardController : Controller
     {
         private readonly IProjectService _projectService;
-        private readonly ITaskListService _taskListService;
-        private readonly ITaskService _taskService;
 
-        public DashboardController(IProjectService projectService, ITaskListService taskListService, ITaskService taskService)
+        public DashboardController(IProjectService projectService)
         {
             _projectService = projectService;
-            _taskListService = taskListService;
-            _taskService = taskService;
         }
 
         public ActionResult Index()
         {
             var projects = _projectService.GetProjects();
             var projectViewModels = Mapper.Map<IEnumerable<Project>, IEnumerable<ProjectViewModel>>(projects);
-
-            foreach (var projectViewModel in projectViewModels)
-            {
-                var taskLists = _taskListService.GetTaskLists(projectViewModel.ProjectId);
-                var taskListViewModels = Mapper.Map<IEnumerable<TaskList>, IEnumerable<TaskListViewModel>>(taskLists);
-
-                foreach (var taskListViewModel in taskListViewModels)
-                {
-                    var tasks = _taskService.GetTasks(taskListViewModel.TaskListId);
-                    taskListViewModel.Tasks = Mapper.Map<IEnumerable<Task>, IEnumerable<TaskViewModel>>(tasks);
-                }
-
-                projectViewModel.TaskLists = taskListViewModels;
-            }
 
             var dashboardViewModel = new DashboardViewModel
             {
