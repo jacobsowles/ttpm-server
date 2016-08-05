1ee5496beb0ec3cd919381408d49ccbf69f258a2
jacob.sowles <jacob.sowles@speakcreative.com>
2016-08-04 17:29:16 -0700

    feat: add toggle between edit and view modes for task table

--- TinyTwoProjectManager.Web/Scripts/task.js
+++ TinyTwoProjectManager.Web/Scripts/task.js
@@ -1,3 +1,21 @@
 ﻿$(function () {
-    
+    $(document).on('click', '#task-table .view-mode input[type=text]', function () {
+        var row = $(this).closest('tr');
+        row.addClass('edit-mode');
+        row.removeClass('view-mode');
+    });
+
+    $(document).on('click', '#task-table .edit-mode input[type=text]', function () {
+        var row = $(this).closest('tr');
+        row.addClass('view-mode');
+        row.removeClass('edit-mode');
+    });
+
+    $(document).on('blur', '#task-table .edit-mode input[type=text]', function () {
+        var row = $(this).closest('tr');
+        row.addClass('view-mode');
+        row.removeClass('edit-mode');
+
+        // TODO: save data
+    });
 });
