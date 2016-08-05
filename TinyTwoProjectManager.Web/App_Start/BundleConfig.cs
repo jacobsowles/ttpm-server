29aab39ba324b183c4903e7a662731419907d5f5
jacob.sowles <jacob.sowles@speakcreative.com>
2016-08-04 17:00:48 -0700

    chore: change bundling lists to use wildcards

--- TinyTwoProjectManager.Web/App_Start/BundleConfig.cs
+++ TinyTwoProjectManager.Web/App_Start/BundleConfig.cs
@@ -6,35 +6,25 @@ namespace TinyTwoProjectManager.Web.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // If these aren't explicitly listed, they won't be loaded in the right order.
             bundles.Add(new ScriptBundle("~/ScriptBundles/ThirdParty")
                 .Include("~/Scripts/ThirdParty/jquery-{version}.min.js")
                 .Include("~/Scripts/ThirdParty/jquery-ui-{version}.min.js")
                 .Include("~/Scripts/ThirdParty/jquery-validate-{version}.min.js")
                 .Include("~/Scripts/ThirdParty/jquery-modal.min.js")
                 .Include("~/Scripts/ThirdParty/bootstrap.min.js")
-                .Include("~/Scripts/ThirdParty/sortable.js")
             );
 
             bundles.Add(new ScriptBundle("~/ScriptBundles/Site")
-                .Include("~/Scripts/accordion.js")
-                .Include("~/Scripts/project.js")
-                .Include("~/Scripts/task.js")
-                .Include("~/Scripts/task-list.js")
+                .Include("~/Scripts/*.js")
             );
 
             bundles.Add(new StyleBundle("~/StyleBundles/ThirdParty")
-                .Include("~/Content/Styles/ThirdParty/bootstrap.min.css")
-                .Include("~/Content/Styles/ThirdParty/bootstrap-theme.min.css")
-                .Include("~/Content/Styles/ThirdParty/jquery-modal.min.css")
+                .Include("~/Content/Styles/ThirdParty/*.min.css")
             );
 
             bundles.Add(new StyleBundle("~/StyleBundles/Site")
-                .Include("~/Content/Styles/global.css")
-                .Include("~/Content/Styles/structure.css")
-                .Include("~/Content/Styles/modules.css")
-                .Include("~/Content/Styles/accordion.css")
-                .Include("~/Content/Styles/modal.css")
-                .Include("~/Content/Styles/project-list.css")
+                .Include("~/Content/Styles/*.css")
             );
 
             BundleTable.EnableOptimizations = true;
