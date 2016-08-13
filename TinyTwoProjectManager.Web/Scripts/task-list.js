8b5263e8d17e9b0d0f513420a3ad147bc3ef7a0d
jacob.sowles <jacob.sowles@speakcreative.com>
2016-08-13 09:39:48 -0700

    feat: give selected task list an active UI state

--- TinyTwoProjectManager.Web/Scripts/task-list.js
+++ TinyTwoProjectManager.Web/Scripts/task-list.js
@@ -1,7 +1,13 @@
 ﻿$(function () {
-    $('.task-lists h4').on('click', function (evt) {
-        evt.preventDefault();
+    $('.task-lists h4').on('click', function (e) {
+        e.preventDefault();
         var taskListId = $(this).attr('id');
+
+        $('.task-lists').children('h4').each(function () {
+            $(this).removeClass('active-task-list');
+        });
+
+        $(this).addClass('active-task-list');
 
         $.get('/Task/GetTasksForTaskList/', { id: taskListId }, function (data) {
             $('#center-column').html(data);
