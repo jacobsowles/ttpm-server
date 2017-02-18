a5354bd101ba7e4068205895daf3ea97a718364b
Jacob Sowles <jacob.sowles@speakcreative.com>
2017-02-17 16:02:31 -0800

    feat: return errors in response

--- src/components/task/task.routes.js
+++ src/components/task/task.routes.js
@@ -5,23 +5,23 @@ const Task = require('../task/task.model');
 module.exports = function(app, passport) {
     const routePrefix = apiConfig.apiPrefix + '/tasks';
 
-    app.get(routePrefix + '/', (request, response, next) => {
+    app.get(routePrefix + '/', (request, response) => {
         Task
             .find()
             .lean()
             .then(tasks => response.send(tasks))
             .catch(error => {
-                next(error);
+                response.send(error);
             });
     });
 
-    app.post(routePrefix + '/', (request, response, next) => {
+    app.post(routePrefix + '/', (request, response) => {
         new Task(request.body)
             .save()
             .then(task => response.send(task))
             .catch(error => {
                 EventLogger.logError(error, 'POST ' + routePrefix + '/', request.body.user);
-                next(error);
+                response.send(error);
             });
     });
 };
