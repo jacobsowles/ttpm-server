49b7ff0b911e16f9366140692f980bff4bd4205b
Jacob Sowles <jacob.sowles@speakcreative.com>
2017-03-10 10:19:33 -0800

    chore: update db config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    uri: 'mongodb://jrsowles:IAmN0rse@ds133398.mlab.com:33398/flipper',
+    uri: process.env.NODE_ENV === 'production' ? 'mongodb://jrsowles:IAmN0rse@ds149049.mlab.com:49049/ttpm' : 'mongodb://jrsowles:IAmN0rse@ds153729.mlab.com:53729/ttpm-test',
     options: {
         server: {
             socketOptions: {
